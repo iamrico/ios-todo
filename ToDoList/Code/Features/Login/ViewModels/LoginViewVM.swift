5fd35886c7a8f71002b613cc125ecea04b447d5b
Rico <rico.mesina@raspect.ai>
2024-05-08 21:55:38 +0800

    updating folder directory structure

--- ToDoList/Code/Features/Login/ViewModels/LoginViewVM.swift
+++ ToDoList/Code/Features/Login/ViewModels/LoginViewVM.swift
@@ -19,22 +19,28 @@ class LoginViewVM: ObservableObject {
     }
     
     func login() {
+        guard isValid() else {
+            return
+        }
+        
+        //
+        
+        Auth.auth().signIn(withEmail: email, password: password)
+    }
+    
+    private func isValid() -> Bool {
         errorMessage = ""
         
         guard !email.trimmingCharacters(in: .whitespaces).isEmpty, !password.trimmingCharacters(in: .whitespaces).isEmpty else {
             errorMessage = "Please fill in all fields"
-            return
+            return false
         }
         
         guard email.contains("@") && email.contains(".") else {
             errorMessage = "Please enter valid email"
-            return
+            return false
         }
         
-        print("Called")
-    }
-    
-    func validate() {
-        
+        return true
     }
 }
